3306fcf3e66fde361234ed8ae58e9789ff54a1e8
Maxrrrrr <pavliv.maxym@gmail.com>
2024-04-14 22:17:25 +0300

    gtertgr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,18 +2,18 @@ import './FriendList.module.css'
 import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
-  return (
-    <ul>
-      {friends.map(friend => (
-        <li key={friend.id}>
-          <FriendListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
-        </li>
-      ))}
-    </ul>
+    return (
+      <ul className={css.friend}>
+        {friends.map(friend => (
+          <li className={css.items} key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        ))}
+      </ul>
   );
 };
 
