39d0809b433682963f1bf533f1bec32086f69b60
Maxrrrrr <pavliv.maxym@gmail.com>
2024-04-15 17:18:43 +0300

    cxz

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,22 @@
-import css from './FriendListItem.module.css'
+import style from './FriendListItem.module.css';
+import clsx from 'clsx';
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <div className={css.FriendListItem}>
-          <img className={css.friendAvatar} src={avatar} alt="Avatar" width="48" />
-          <p className={css.friendName}>{name}Friend name</p>
-          <p className={css.friendStatus}>{isOnline}Friend status</p>
-</div>
-  );
-};
+const FriendListItem = (
+    {
+        avatar,
+        name,
+        isOnline
+    }
+) => {
+    const isOnlineSts = clsx(style.isOnlineStatus,
+        isOnline ? style.friendIsOnline : style.friendIsOffline);
+    return (
+        <div className={style.friendItem}>
+            <img className={style.friendAvatar} src={avatar} alt="Avatar" width="84" />
+            <p className={style.friendName}>{name}</p>
+            <p className={isOnlineSts}>{isOnline ? 'Online' : 'Offline'}</p>
+        </div>
+    );
+}
 
 export default FriendListItem;
